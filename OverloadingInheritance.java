cad83831686bcdfdaba550b71e134893a12b835d
neelima2004 <158811525+neelima2004@users.noreply.github.com>
2024-03-17 20:47:05 +0530

    Create OverloadingInheritance.java

---
+++ OverloadingInheritance.java
@@ -0,0 +1,23 @@
+class Mul2int
+{
+    int multiply(int a,int b)
+    {
+        return a*b;
+    }
+}
+class Mul3int extends Mul2int
+{
+    int multiply(int a,int b,int c)
+    {
+        return a*b*c;
+    }
+}
+public class OverloadingInheritance
+{
+    public static void main(String[] args)
+    {
+        Mul3int obj=new Mul3int();
+        System.out.println("The product of two numbers : "+obj.multiply(2,4));
+        System.out.println("The product of three numbers : "+obj.multiply(2,6,3));
+    }
+}
