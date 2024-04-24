fe74d3b27d67e5eff1e7832dc7cdb1c8eb0f1af2
neelima2004 <158811525+neelima2004@users.noreply.github.com>
2024-04-24 21:08:40 +0530

    Create genericsJava.java

---
+++ genericsJava.java
@@ -0,0 +1,33 @@
+class bSort<T extends Comparable<T>>
+{
+    int i=0;
+    int j=0;
+    void sort(T[] data)
+    {
+        for(i=0;i<data.length;i++)
+        {
+            for(j=i+1;j<data.length;j++)
+            {
+                if(data[i].compareTo(data[j])>0)
+                {
+                    T temp=data[i];
+                    data[i]=data[j];
+                    data[j]=temp;
+                }
+            }
+        }
+        for(i=0;i<data.length;i++)
+        {
+            System.out.println(data[i]);
+        }
+    }
+}
+public class genericsJava
+{
+    public static void main(String[] args)
+    {
+        bSort<String> obj=new bSort<String>();
+        String[] arr={"k","v","n","d","a","g"};
+        obj.sort(arr);
+    }
+}
